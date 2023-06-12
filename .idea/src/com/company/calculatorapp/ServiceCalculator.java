c6f66767926a9e2ae19a5861c4efd8fd39a8a2e5
Sardorbek Murodov <smurodov870@gmail.com>
2023-06-12 20:45:44 +0500

    "some bugs in variable types are fixed"

--- .idea/src/com/company/calculatorapp/ServiceCalculator.java
+++ .idea/src/com/company/calculatorapp/ServiceCalculator.java
@@ -3,17 +3,22 @@ package com.company.calculatorapp;
 public class ServiceCalculator {
     public void add(double number1, double number2) {
         //todo
+        System.out.println("add method");
+
     }
 
     public void subtract(double number1, double number2) {
         //todo
+        System.out.println("subtract method ");
     }
 
     public void multiply(double number1, double number2) {
         //todo
+        System.out.println("multiple method");
     }
 
     public void divide(double number1, double number2) {
         //todo
+        System.out.println("divide method");
     }
 }
