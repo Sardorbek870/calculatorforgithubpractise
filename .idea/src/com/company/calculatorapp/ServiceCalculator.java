ebd4b58173db009219dc22873f6cfd22d26490ae
HDavron <hdavron7@gmail.com>
2023-06-13 08:14:01 +0500

    Change 2

--- .idea/src/com/company/calculatorapp/ServiceCalculator.java
+++ .idea/src/com/company/calculatorapp/ServiceCalculator.java
@@ -1,13 +1,14 @@
 package com.company.calculatorapp;
 
 public class ServiceCalculator {
-    public double add(double number1, double number2) {
+    public void add(double number1, double number2) {
         System.out.println("add method");
-        return number1 + number2;
+        System.out.println(number1 + number2);
     }
 
     public void subtract(double number1, double number2) {
         System.out.println("subtract method ");
+        System.out.println(number1 - number2);
     }
 
     public void multiply(double number1, double number2) {
